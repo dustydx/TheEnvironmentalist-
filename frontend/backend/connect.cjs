8d90924a911706b251cf34165e2e15fedfeafd86
Li Yuan Ho <liyuanh@usc.edu>
2025-06-25 10:56:15 -0700

    Adding backend folder

--- frontend/backend/connect.cjs
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("mongodb")
-require ("dotenv"). config({path: "./config-env"})
-
-async function main() {
-    const Db = process.env.ATLAS_URI
-    const client = new MongoClient(Db)
-    await client. connect()
-}
