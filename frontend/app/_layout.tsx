0d7b692d57f4d1813c58cf6e1f97ad0c95233f7a
dazyfrgso <daisyfregoso059@gmail.com>
2025-06-26 12:38:42 -0700

    new update

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,1 @@
-import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="home screen" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
